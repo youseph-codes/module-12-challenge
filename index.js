a40d0c37379f90940e8b8bc3f67967750f6229f0
Yousopp <yousephagueye@gmail.com>
2022-10-30 17:07:25 -0400

    updated viewDepartments function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./connection/connection')
 const apiRoutes = require('')
 
-const mainQuestions = [
+const menuQuestions = [
     {
         type: 'list',
         name: 'answer',
@@ -19,3 +19,20 @@ const mainQuestions = [
     },
 ];
 
+// viewing functions of departments, roles, and employees
+function viewDepartments() {
+    db.promise()
+    .query('SELECT departments.id, departments.department_name FROM departments;')
+    .then((departments) => {
+        console.table(departments[0]);
+        menuQuestions();
+    });
+}
+
+function viewRoles() {
+    db.promise()
+}
+
+function viewEmployees() {
+    db.promise()
+}
