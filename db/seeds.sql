234b31ee7a4c78a766ea26c660fac1ed77987b8c
Yousopp <yousephagueye@gmail.com>
2022-10-29 20:53:15 -0400

    updated seeds.schema

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal'),
+
+INSERT INTO roles
+VALUES
+('Sales Lead')
+('Salesperson')
+('Lead Engineer')
+('Software Engineer')
+('Account Manager')
+('Accountant')
+('Legal Team Lead')
+('Lawyer')
+
+INSERT INTO employees
+VALUES
+(),
+(),
+(),
+(),
+(),
+(),
+(),
+(),
